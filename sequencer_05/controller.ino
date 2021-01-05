e522ce9bfd17429a8ebf4d61a248e2daf34b2753
Alexandre Piel <alexandre.piel@gmail.com>
2021-01-05 19:58:05 +0100

    menu selector

---
+++ sequencer_05/controller.ino
@@ -0,0 +1,199 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+
+// OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+#ifdef WEMOS_BOARD
+#define PIN_BTN_1 5   // d1
+#define PIN_BTN_2 16  // d0
+#define PIN_BTN_3 0   // d3
+#else
+#define PIN_BTN_1 39
+#define PIN_BTN_2 38
+#define PIN_BTN_3 0
+#endif
+
+#define OPTION_COUNT 8
+
+typedef struct {
+    char* name;
+    bool isCategory = false;
+} Option;
+
+Option gOption[OPTION_COUNT];
+
+unsigned long gBtnTimeLast = 0;
+unsigned long gBtnTimeLast2 = 0;
+unsigned int gSelected = 0;
+
+#define SEL_MOD_MENU 1
+#define SEL_MOD_VALUE 2
+#define SEL_MOD_CATEGORY 3
+byte gSelMod = SEL_MOD_MENU;
+
+void controllerSetup() {
+    pinMode(PIN_BTN_1, INPUT);
+    pinMode(PIN_BTN_2, INPUT);
+    pinMode(PIN_BTN_3, INPUT);
+
+    // reset OLED display via software
+    pinMode(OLED_RST, OUTPUT);
+    digitalWrite(OLED_RST, LOW);
+    delay(20);
+    digitalWrite(OLED_RST, HIGH);
+
+    // initialize OLED
+    Wire.begin(OLED_SDA, OLED_SCL);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false,
+                       false)) {  // Address 0x3C for 128x32
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ;  // Don't proceed, loop forever
+    }
+
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.print("SEQUENCER");
+    display.display();
+
+    gOption[0].isCategory = true;
+    gOption[0].name = "Play";
+    gOption[1].name = "BPM";
+    gOption[2].name = "Sequence gate";
+    gOption[3].name = "Pattern";
+    gOption[4].isCategory = true;
+    gOption[4].name = "yo";
+    gOption[5].name = "abc";
+    gOption[6].name = "hihi";
+    gOption[7].name = "hiho";
+
+    updateDisplay();
+}
+
+void controllerHandler() {
+    setSelector();
+    setValue();
+}
+
+bool isClick(int pin, int ms = 100, int val = HIGH) {
+    if (millis() - gBtnTimeLast > ms) {
+        int btn = digitalRead(pin);
+        if (btn == val) {
+            gBtnTimeLast = millis();
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isClick2(int pin, int ms = 200, int val = HIGH) {
+    if (millis() - gBtnTimeLast2 > ms) {
+        int btn = digitalRead(pin);
+        if (btn == val) {
+            gBtnTimeLast2 = millis();
+            return true;
+        }
+    }
+    return false;
+}
+
+int mod(int n, int m) { return ((n % m) + m) % m; }
+
+void updateDisplay() {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    if (gSelMod == SEL_MOD_MENU) {
+        display.println("# menu #");
+    } else if (gSelMod == SEL_MOD_VALUE) {
+        display.println("# value #");
+    } else {
+        display.println("# category #");
+    }
+    display.println(gOption[mod(gSelected - 3, OPTION_COUNT)].name);
+    display.println(gOption[mod(gSelected - 2, OPTION_COUNT)].name);
+    display.println(gOption[mod(gSelected - 1, OPTION_COUNT)].name);
+    display.print(">");
+    display.println(gOption[gSelected].name);
+    display.println(gOption[mod(gSelected + 1, OPTION_COUNT)].name);
+    display.println(gOption[mod(gSelected + 2, OPTION_COUNT)].name);
+    display.println(gOption[mod(gSelected + 3, OPTION_COUNT)].name);
+    display.display();
+}
+
+void setSelector() {
+    if (isClick(PIN_BTN_3, 300, LOW)) {
+        // if second click is under 2x300 then it is double click
+        if (!isClick2(PIN_BTN_3, 600, LOW)) {
+            gSelMod = SEL_MOD_CATEGORY;
+            Serial.println("double click");
+        } else {
+            if (gSelMod == SEL_MOD_CATEGORY || gSelMod == SEL_MOD_VALUE) {
+                gSelMod = SEL_MOD_MENU;
+            } else {
+                gSelMod = SEL_MOD_VALUE;
+            }
+            Serial.println("click");
+        }
+        Serial.print("sel mod ");
+        Serial.println(gSelMod);
+        updateDisplay();
+    } else if (gSelMod == SEL_MOD_MENU) {
+        bool btn1 = isClick(PIN_BTN_1, 300);
+        bool btn2 = isClick(PIN_BTN_2, 300);
+
+        if (btn1 || btn2) {
+            int val = btn1 ? 1 : -1;
+            gSelected = mod(gSelected + val, OPTION_COUNT);
+            Serial.print("Sel ");
+            Serial.println(gOption[gSelected].name);
+            updateDisplay();
+        }
+    }
+}
+
+void setValue() {
+    if (gSelMod == SEL_MOD_VALUE) {
+        bool btn1 = isClick(PIN_BTN_1);
+        bool btn2 = isClick(PIN_BTN_2);
+
+        if (btn1 || btn2) {
+            int val = btn1 ? 1 : -1;
+
+            if (gSelected == 0) {
+                gSeqPlay = btn1;
+                Serial.print("Play: ");
+                Serial.println(gSeqPlay);
+            } else if (gSelected == 1) {
+                int bpm = gSeqBase.gSeqBPM + val;
+                if (bpm > 10 && bpm < 250) {
+                    gSeqBase.gSeqBPM = bpm;
+                    setTempo();
+                }
+                Serial.print("set BPM: ");
+                Serial.println(gSeqBase.gSeqBPM);
+            } else if (gSelected == 2) {
+                int pct = gSeqBase.gSeqGatePercent + val;
+                if (pct > 0 && pct < 99) {
+                    gSeqBase.gSeqGatePercent = pct;
+                    calcGate();
+                }
+                Serial.print("set sequence gate percentage: ");
+                Serial.println(gSeqBase.gSeqGatePercent);
+            } else if (gSelected == 3) {
+                gSeqPatternIndex = (gSeqPatternIndex + val) % MAX_PATTERNS;
+                Serial.print("set pattern: ");
+                Serial.println(gSeqPatternIndex);
+            }
+        }
+    }
+}
