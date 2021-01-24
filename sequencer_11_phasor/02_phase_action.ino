48d0990c62c4a8dbcaee4f425a7ab401603a7ee0
alex <alexandre.piel@gmail.com>
2021-01-24 19:14:46 +0100

    adsrfreq

--- sequencer_11_phasor/02_phase_action.ino
+++ sequencer_11_phasor/02_phase_action.ino
@@ -57,75 +57,74 @@ void setPhaseFromMidiKnob(byte phaseIdx, byte optionKey, int direction) {
         currentTableId = mod(currentTableId + direction, 20);
         setTable(phaseIdx, currentTableId);
     } else if (optionKey == 5) {
-        phase->adsr.setAttackTime(
-            incTime(phase->adsr.getTime(ATTACK), direction));
+        if (gAdsrFreqSetMode) {
+            phase->adsrFreq.setAttackTime(
+                incTime(phase->adsrFreq.getTime(ATTACK), direction));
+        } else {
+            phase->adsr.setAttackTime(
+                incTime(phase->adsr.getTime(ATTACK), direction));
+        }
     } else if (optionKey == 6) {
-        phase->adsr.setDecayTime(
-            incTime(phase->adsr.getTime(DECAY), direction));
+        if (gAdsrFreqSetMode) {
+            phase->adsrFreq.setDecayTime(
+                incTime(phase->adsrFreq.getTime(DECAY), direction));
+        } else {
+            phase->adsr.setDecayTime(
+                incTime(phase->adsr.getTime(DECAY), direction));
+        }
     } else if (optionKey == 7) {
-        phase->adsr.setSustainTime(
-            incTime(phase->adsr.getTime(SUSTAIN), direction));
+        if (gAdsrFreqSetMode) {
+            phase->adsrFreq.setSustainTime(
+                incTime(phase->adsrFreq.getTime(SUSTAIN), direction));
+        } else {
+            phase->adsr.setSustainTime(
+                incTime(phase->adsr.getTime(SUSTAIN), direction));
+        }
+
     } else if (optionKey == 8) {
-        phase->adsr.setReleaseTime(
-            incTime(phase->adsr.getTime(RELEASE), direction));
+        if (gAdsrFreqSetMode) {
+            phase->adsrFreq.setReleaseTime(
+                incTime(phase->adsrFreq.getTime(RELEASE), direction));
+        } else {
+            phase->adsr.setReleaseTime(
+                incTime(phase->adsr.getTime(RELEASE), direction));
+        }
     } else if (optionKey == 15) {
-        phase->adsr.setAttackLevel(
-            incLevel(phase->adsr.getLevel(ATTACK), direction));
+        if (gAdsrFreqSetMode) {
+            phase->adsrFreq.setAttackLevel(
+                incLevel(phase->adsrFreq.getLevel(ATTACK), direction));
+        } else {
+            phase->adsr.setAttackLevel(
+                incLevel(phase->adsr.getLevel(ATTACK), direction));
+        }
+
     } else if (optionKey == 16) {
-        phase->adsr.setDecayLevel(
-            incLevel(phase->adsr.getLevel(DECAY), direction));
+        if (gAdsrFreqSetMode) {
+            phase->adsrFreq.setDecayLevel(
+                incLevel(phase->adsrFreq.getLevel(DECAY), direction));
+        } else {
+            phase->adsr.setDecayLevel(
+                incLevel(phase->adsr.getLevel(DECAY), direction));
+        }
+
     } else if (optionKey == 17) {
-        phase->adsr.setSustainLevel(
-            incLevel(phase->adsr.getLevel(SUSTAIN), direction));
+        if (gAdsrFreqSetMode) {
+            phase->adsrFreq.setSustainLevel(
+                incLevel(phase->adsrFreq.getLevel(SUSTAIN), direction));
+        } else {
+            phase->adsr.setSustainLevel(
+                incLevel(phase->adsr.getLevel(SUSTAIN), direction));
+        }
+
     } else if (optionKey == 18) {
-        phase->adsr.setReleaseLevel(
-            incLevel(phase->adsr.getLevel(RELEASE), direction));
+        if (gAdsrFreqSetMode) {
+            phase->adsrFreq.setReleaseLevel(
+                incLevel(phase->adsrFreq.getLevel(RELEASE), direction));
+        } else {
+            phase->adsr.setReleaseLevel(
+                incLevel(phase->adsr.getLevel(RELEASE), direction));
+        }
     }
-
-    // else if (optionKey == 3) {
-    //     gPhase[phase].PeakLevel =
-    //         between(gPhase[phase].PeakLevel + direction, 0, 250);
-    // } else if (optionKey == 4) {
-    //     gPhase[phase].SustainLevel =
-    //         between(gPhase[phase].SustainLevel + direction, 0, 250);
-    // } else if (optionKey == 5) {
-    //     gPhase[phase].ATime = between(gPhase[phase].ATime + direction, 0,
-    //     100);
-    // } else if (optionKey == 6) {
-    //     gPhase[phase].DTime = between(gPhase[phase].DTime + direction, 0,
-    //     100);
-    // } else if (optionKey == 7) {
-    //     gPhase[phase].STime = between(gPhase[phase].STime + direction, 0,
-    //     100);
-    // } else if (optionKey == 8) {
-    //     gPhase[phase].RTime = between(gPhase[phase].RTime + direction, 0,
-    //     100);
-    // } else if (optionKey == 12) {
-    //     gPhase[phase].frequency =
-    //         between(gPhase[phase].frequency + direction, 0, 2000);
-    // } else if (optionKey == 13) {
-    //     gPhase[phase].AFreqLevel =
-    //         between(gPhase[phase].AFreqLevel + direction, 0, 255);
-    // } else if (optionKey == 14) {
-    //     gPhase[phase].SFreqLevel =
-    //         between(gPhase[phase].SFreqLevel + direction, 0, 255);
-    // } else if (optionKey == 15) {
-    //     gPhase[phase].RFreqLevel =
-    //         between(gPhase[phase].RFreqLevel + direction, 0, 255);
-    // } else if (optionKey == 16) {
-    //     gPhase[phase].AFreqTime =
-    //         between(gPhase[phase].AFreqTime + direction, 0, 100);
-    // } else if (optionKey == 17) {
-    //     gPhase[phase].SFreqTime =
-    //         between(gPhase[phase].SFreqTime + direction, 0, 100);
-    // } else if (optionKey == 18) {
-    //     gPhase[phase].RFreqTime =
-    //         between(gPhase[phase].RFreqTime + direction, 0, 100);
-    // }
-
-    // if (optionKey != 2 && optionKey != 12) {
-    //     applySetting(phase);
-    // }
 }
 
 void playPhase() {
