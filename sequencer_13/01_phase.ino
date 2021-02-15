b2c12640db77b44309ac23eaf1665d7af60738a0
alex <alexandre.piel@gmail.com>
2021-02-15 19:40:48 +0100

    seq_13

---
+++ sequencer_13/01_phase.ino
@@ -0,0 +1,140 @@
+/*
+Make the frequency adsr table independent from the main adsr
+So the frequency adsr doesnt have to be the same...
+
+when pressing loop the pattern button become synth
+
+Look at PDResonant it seem awesome :p
+Look at synth in general:
+- FMsynth
+- WaveShaper?
+*/
+
+#include <LowPassFilter.h>
+
+#include "Phase.h"
+
+#define D_KICK 1
+#define D_SNARE 2
+#define D_HIHAT 4
+#define D_CLAP 8
+#define D_CRASH 16
+#define D_PHASOR 32
+
+#define MAX_NUM_CELLS 8192
+
+// seem to be kind of useless
+LowPassFilter lpf;
+byte gCutoff = 255;
+byte gResonance = 0;
+
+byte currentTableId = 0;
+
+byte gSeqPhases[MAX_PATTERNS][STEP_COUNT] = {
+    {D_KICK, 0, 0, 0, D_SNARE, 0, 0, 0, D_KICK, 0, 0, 0, D_SNARE, 0, 0, D_KICK},
+    {D_KICK + D_CRASH, 0, D_HIHAT, 0, D_KICK, 0, D_HIHAT, 0, D_KICK, 0, D_HIHAT,
+     0, D_CLAP, 0, D_HIHAT, D_KICK},
+    {D_KICK, 0, 0, 0, D_KICK, 0, 0, 0, D_KICK, 0, 0, 0, D_KICK, 0, 0, 0},
+    {D_PHASOR, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+byte gCurrentPattern[STEP_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0,
+                                    0, 0, 0, 0, 0, 0, 0, 0};
+
+Phase<MAX_NUM_CELLS, STEP_COUNT> phases[MAX_PHASES];
+
+void setupPhase(byte phase, byte tableId, byte type, int frequency) {
+    const float adsrTimes[] = {0.0, 0.0, 0.0, 1.0};
+    const byte adsrLevels[] = {200, 200, 200, 0};
+    setupPhase(phase, tableId, type, frequency, adsrTimes, adsrLevels);
+}
+
+void setupPhase(byte phase, byte tableId, byte type, int frequency,
+                const float adsrTimes[4], const byte adsrLevels[4]) {
+    const float adsrFreqTimes[] = {0.0, 0.0, 0.0, 1.0};
+    const byte adsrFreqLevels[] = {200, 200, 200, 0};
+    setupPhase(phase, tableId, type, frequency, adsrTimes, adsrLevels,
+               adsrFreqTimes, adsrFreqLevels);
+}
+
+void setupPhase(byte phase, byte tableId, byte type, int frequency,
+                const float adsrTimes[4], const byte adsrLevels[4],
+                const float adsrFreqTimes[4], const byte adsrFreqLevels[4]) {
+    setTable(phase, tableId);
+
+    lpf.setCutoffFreqAndResonance(gCutoff, gResonance);
+
+    phases[phase].setType(type);
+    phases[phase].frequency = frequency;
+
+    phases[phase].adsr.setTimes(adsrTimes[0] * gTempo, adsrTimes[1] * gTempo,
+                                adsrTimes[2] * gTempo, adsrTimes[3] * gTempo);
+    phases[phase].adsr.setLevels(adsrLevels[0], adsrLevels[1], adsrLevels[2],
+                                 adsrLevels[3]);
+
+    phases[phase].adsrFreq.setTimes(
+        adsrFreqTimes[0] * gTempo, adsrFreqTimes[1] * gTempo,
+        adsrFreqTimes[2] * gTempo, adsrFreqTimes[3] * gTempo);
+    phases[phase].adsrFreq.setLevels(adsrFreqLevels[0], adsrFreqLevels[1],
+                                     adsrFreqLevels[2], adsrFreqLevels[3]);
+}
+
+#define MAX_PHASES_SETUP 2
+
+void setupPhases(byte phasesSetupId) {
+    gCurrentPhasesSetup = mod(phasesSetupId, MAX_PHASES_SETUP);
+
+    if (gCurrentPhasesSetup == 0) {
+        setupPhase(0, getTablesCount(), FREQ_ENV, 45);
+        // phases[0].freqSteps[0] = 100;
+        // phases[0].freqSteps[4] = 400;
+        // phases[0].freqSteps[8] = 200;
+        // phases[0].freqSteps[12] = -300;
+
+        // setupPhase(1, 20, PHASOR2, 150);
+        setupPhase(1, getTablesCount(), FREQ_ENV, 150);
+        setupPhase(2, 10, SIMPLE, 2, (const float[4]){0.0, 0.0, 0.0, 0.3},
+                   (const byte[4]){100, 100, 100, 0});
+        setupPhase(3, 10, SIMPLE, 0);
+        setupPhase(4, 10, SIMPLE, 0);
+        setupPhase(5, getTablesCount(), PHASOR3, 30, (const float[4]){1.0, 1.0, 12.0, 2.0},
+                   (const byte[4]){70, 70, 70, 0},
+                   (const float[4]){3.0, 5.0, 8.0, 2.0},
+                   (const byte[4]){255, 100, 100, 0});
+        phases[5].adsrFreq.setLerpRate(CONTROL_RATE);
+    } else {
+        setupPhase(0, 9, FREQ_ENV, 45);
+        setupPhase(1, getTablesCount(), PHASOR2, 150);
+        setupPhase(2, 10, SIMPLE, 100);
+        setupPhase(3, 4, SIMPLE, 0);
+        setupPhase(4, 10, SIMPLE, 0);
+        setupPhase(5, 18, PHASOR3, 30);
+    }
+}
+
+void playPhase() {
+    int aPhase = gCurrentPattern[gSeqStepIndex];
+
+    for (int i = 0; i < MAX_PHASES; i++) {
+        if (aPhase & (int)pow(2, i)) {
+            phases[i].noteOn(gSeqStepIndex);
+        }
+    }
+}
+
+void updateEnvelopes() {
+    for (int i = 0; i < MAX_PHASES; i++) {
+        phases[i].update();
+    }
+}
+
+int updateAudioSeq() {
+    int ret = 0;
+    for (int i = 0; i < MAX_PHASES; i++) {
+        ret += phases[i].next();
+    }
+
+    // return ret >> 8;
+    return lpf.next((int)(ret * gVolume / MAX_VOLUME)) >> 8;
+    // return (int)(ret * gVolume /
+    // MAX_VOLUME) >> 8;
+}
---
+++ sequencer_13/20_display.ino
@@ -0,0 +1,281 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+
+// OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
+#define SCREEN_W 128  // OLED display width, in pixels
+#define SCREEN_H 64   // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_W, SCREEN_H, &Wire, OLED_RST);
+
+char buf[SCREEN_W];
+
+EventDelay displayDelay;
+
+void displaySetup() {
+    // reset OLED display via software
+    pinMode(OLED_RST, OUTPUT);
+    digitalWrite(OLED_RST, LOW);
+    delay(20);
+    digitalWrite(OLED_RST, HIGH);
+
+    // initialize OLED
+    Wire.begin(OLED_SDA, OLED_SCL);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false,
+                       false)) {  // Address 0x3C for 128x32
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ;  // Don't proceed, loop forever
+    }
+
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.println("SEQUENCER");
+
+    displayUpdate();
+}
+
+void displayUpdate() {
+    if (displayDelay.ready()) {
+        if (isPhaseMenu()) {
+            displayPhase();
+        } else {
+            displayMainMenu();
+        }
+        displayStatus();
+        display.display();
+        // displayDelay.start(150);
+        displayDelay.start(gTempo / 2);
+    }
+}
+
+void displayStatus() {
+    if (gSeqPlay) {
+        display.fillTriangle(100, 0, 106, 3, 100, 7, WHITE);
+    } else {
+        display.fillRect(100, 1, 6, 6, WHITE);
+    }
+    // dprintxy(18, 0, "%d", gSeqStepIndex+1);
+    for (byte i = 0; i < gSeqStepIndex + 1; i++) {
+        // display.fillRect(68 + (i%8) * 4, i >= 8 ? 0 : 5, 2, 2, WHITE);
+        byte y = 0;
+        if (i > 11)
+            y = 6;
+        else if (i > 7)
+            y = 4;
+        else if (i > 3)
+            y = 2;
+        display.fillRect(82 + (i % 4) * 4, y, 2, 1, WHITE);
+    }
+    display.drawTriangle(110, 6, 127, 6, 127, 0, WHITE);
+    display.fillTriangle(110, 6, 110 + (17 * gVolume / MAX_VOLUME), 6,
+                         110 + (17 * gVolume / MAX_VOLUME),
+                         6 - (6 * gVolume / MAX_VOLUME), WHITE);
+
+    if (gMcMode) {
+        display.setCursor(66, 0);
+        display.setTextColor(BLACK, WHITE);
+        display.print("mc");
+        display.setTextColor(WHITE);
+        display.drawLine(65, 0, 65, 7, WHITE);
+    }
+}
+
+void displayMainMenu() {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Main menu");
+    display.println("");
+    dprintln("BPM: %d", gBPM);
+    dprintln("Pattern: %d", gCurrentPatternId);
+    dprintln("Setup: %d", gCurrentPhasesSetup);
+    displayLpf();
+}
+
+Phase<MAX_NUM_CELLS, STEP_COUNT> *getCurrentPhase() {
+    return &phases[getCurrentPhaseIdx()];
+}
+
+void displayPhase() {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    dprintln("Phase %c", getCurrentPhaseChar());
+
+    Phase<MAX_NUM_CELLS, STEP_COUNT> *phase = getCurrentPhase();
+
+    display.println(phase->tableName);
+    dprintln("Freq %d", phase->frequency);
+    if (gMcMode) {
+        dprintln("%d-> %d", gStepMode + 1, phase->freqSteps[gStepMode]);
+    }
+
+    dprintxyAbs(4, 32, "A");
+    dprintxyTimePct(0, 40, phase->adsr.getTime(ATTACK));
+    dprintxyLevelPct(0, 48, phase->adsr.getLevel(ATTACK));
+
+    display.drawLine(15, 32, 15, 55, WHITE);
+
+    dprintxyAbs(21, 32, "D");
+    dprintxyTimePct(17, 40, phase->adsr.getTime(DECAY));
+    dprintxyLevelPct(17, 48, phase->adsr.getLevel(DECAY));
+
+    display.drawLine(31, 32, 31, 55, WHITE);
+
+    dprintxyAbs(37, 32, "S");
+    dprintxyTimePct(33, 40, phase->adsr.getTime(SUSTAIN));
+    dprintxyLevelPct(33, 48, phase->adsr.getLevel(SUSTAIN));
+
+    display.drawLine(47, 32, 47, 55, WHITE);
+
+    dprintxyAbs(53, 32, "R");
+    dprintxyTimePct(49, 40, phase->adsr.getTime(RELEASE));
+    dprintxyLevelPct(49, 48, phase->adsr.getLevel(RELEASE));
+
+    if (phase->type > REVERB) {
+        if (gMcMode) {
+            display.fillTriangle(65, 33, 65, 38, 67, 35, WHITE);
+            display.fillTriangle(82, 33, 82, 38, 84, 35, WHITE);
+            display.fillTriangle(98, 33, 98, 38, 100, 35, WHITE);
+            display.fillTriangle(114, 33, 114, 38, 116, 35, WHITE);
+        } else {
+            display.fillTriangle(0, 33, 0, 38, 2, 35, WHITE);
+            display.fillTriangle(17, 33, 17, 38, 19, 35, WHITE);
+            display.fillTriangle(33, 33, 33, 38, 35, 35, WHITE);
+            display.fillTriangle(49, 33, 49, 38, 51, 35, WHITE);
+        }
+
+        display.drawLine(63, 32, 63, 55, WHITE);
+
+        dprintxyAbs(70, 32, "A");
+        dprintxyTimePct(65, 40, phase->adsrFreq.getTime(ATTACK));
+        dprintxyLevelPct(65, 48, phase->adsrFreq.getLevel(ATTACK));
+
+        display.drawLine(79, 32, 79, 55, WHITE);
+
+        dprintxyAbs(86, 32, "D");
+        dprintxyTimePct(81, 40, phase->adsrFreq.getTime(DECAY));
+        dprintxyLevelPct(81, 48, phase->adsrFreq.getLevel(DECAY));
+
+        display.drawLine(95, 32, 95, 55, WHITE);
+
+        dprintxyAbs(102, 32, "S");
+        dprintxyTimePct(97, 40, phase->adsrFreq.getTime(SUSTAIN));
+        dprintxyLevelPct(97, 48, phase->adsrFreq.getLevel(SUSTAIN));
+
+        display.drawLine(111, 32, 111, 55, WHITE);
+
+        dprintxyAbs(118, 32, "R");
+        dprintxyTimePct(113, 40, phase->adsrFreq.getTime(RELEASE));
+        dprintxyLevelPct(113, 48, phase->adsrFreq.getLevel(RELEASE));
+
+        if (phase->type == FREQ_ENV) {
+            dprintxy(12, 1, "Freq Env");
+            dprintxy(12, 2, "Shift %d", phase->freqShift);
+        } else if (phase->type == PHASOR2) {
+            dprintxy(12, 1, "Phasor 2");
+            dprintxy(12, 2, "Shift %d", phase->phasorShift);
+        } else if (phase->type == PHASOR3) {
+            dprintxy(12, 1, "Phasor 3");
+            dprintxy(12, 2, "Shift %d", phase->phasorShift);
+        }
+        dprintxy(12, 3, "LR %d", phase->adsrFreq.getLerpRate());
+    } else {
+        if (phase->type == REVERB) {
+            dprintxy(12, 1, "Reverb");
+        } else {
+            dprintxy(12, 1, "Simple");
+        }
+    }
+    displayPhasePattern();
+}
+
+void displayPhasePattern() {
+    byte idx = getCurrentPhaseIdx();
+
+    for (byte i = 0, s = 0; i < STEP_COUNT; i++) {
+        if (i % 4 == 0) s += 3;
+        int aPhase = gCurrentPattern[i];
+        if (aPhase & (int)pow(2, idx)) {
+            display.fillRect(i * 7 + s, 57, 6, 6, WHITE);
+        } else {
+            display.drawRect(i * 7 + s, 57, 6, 6, WHITE);
+        }
+        if (i == gSeqStepIndex) {
+            display.drawLine(i * 7 + 1 + s, 63, i * 7 + 4 + s, 63, WHITE);
+        }
+    }
+}
+
+void displayLpf() {
+    dprintln("Cutoff: %d", gCutoff);
+    dprintln("Resonance: %d", gResonance);
+}
+
+void dprintxyLevelPct(byte x, byte y, byte level) {
+    if (level == 255) {
+        display.setCursor(x + 4, y);
+        display.print(1);
+    } else {
+        int value = (float)level / 255 * 100;
+        display.setCursor(x + 2, y);
+        display.print(value);
+        display.drawPixel(x, y + 6, WHITE);
+    }
+}
+
+void dprintxyTimePct(byte x, byte y, unsigned int ms) {
+    float value = (float)ms / gTempo;
+    if (value < 1) {
+        byte f = value * 10;
+        display.setCursor(x + 2, y);
+        display.print(f);
+        display.drawPixel(x, y + 6, WHITE);
+    } else {
+        display.setCursor(x, y);
+        byte i = (byte)value;
+        display.print(i);
+        display.setCursor(x + 8, y);
+        byte f = (value - i) * 10;
+        display.print(f);
+        display.drawPixel(x + 6, y + 6, WHITE);
+    }
+}
+
+void dprintxyAbs(byte x, byte y, const char *str, ...) {
+    display.setCursor(x, y);
+    va_list argptr;
+    va_start(argptr, str);
+    vsnprintf(buf, SCREEN_W, str, argptr);
+    va_end(argptr);
+    display.print(buf);
+}
+
+void dprintxy(byte x, byte y, const char *str, ...) {
+    display.setCursor(6 * x, 8 * y);
+    va_list argptr;
+    va_start(argptr, str);
+    vsnprintf(buf, SCREEN_W, str, argptr);
+    va_end(argptr);
+    display.print(buf);
+}
+
+void dprint(const char *str, ...) {
+    va_list argptr;
+    va_start(argptr, str);
+    vsnprintf(buf, SCREEN_W, str, argptr);
+    va_end(argptr);
+    display.print(buf);
+}
+
+void dprintln(const char *str, ...) {
+    va_list argptr;
+    va_start(argptr, str);
+    vsnprintf(buf, SCREEN_W, str, argptr);
+    va_end(argptr);
+    display.println(buf);
+}
